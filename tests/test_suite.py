ae5582eaa352ee91cbdda64c87d7ee985dff5b4b
Raymond Viviano <rayviviano@gmail.com>
2020-03-09 17:19:12 -0400

    Test vector normalization by maximum value
    Test that arrays are normalized as expected and that data types are correct.
    
    Arrays, representing wavs, are processed on a by-channel basis. That is, the left channel of wav 2 is normalized to have the same maximum as the left channel of wav 1 regardless of whatever is going on with the right channel of wav 1.
    
    Also ensure that the datatype of the resulting array is equivalent to the input array. The division required by normalization will initiallly cast the result to a float type that needs to be downcast to an integer type to be compatible with wavio.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 import os, sys
+import numpy as np
 import unittest
 
 # Get directory location of the test_suite script
@@ -18,3 +19,37 @@ import chainlink as cl
 
 
 # TODO: Write tests
+
+class TestNormalizationMethods(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestNormalizationMethods, self).__init__(*args, **kwargs)
+
+        self.test_chunk1 = np.asarray([[50, 50, 20, 10, 40, 60, 70], 
+                                       [60, 60, 20, 10, 40, 60, 70]], 
+                                      dtype=np.int32).T
+
+        self.test_chunk2 = np.asarray([[50, 52, 20, 13, 42, 60, 100], 
+                                       [60, 77, 20, 27, 40, 61, 100]], 
+                                      dtype=np.int32).T
+                                      
+
+    def test_max_normalization(self):
+        answer = np.asarray([[35, 36, 14, 9,  29, 42, 70], 
+                             [42, 53, 14, 18, 28, 42, 70]], 
+                            dtype=np.int32).T
+
+        result = cl.normalize_chunk(self.test_chunk1, self.test_chunk2, 'Max')
+
+        # Assert that the normalized array appears as expected
+        np.testing.assert_array_equal(result, answer)
+
+        # Assert that the datatype of the new array matches the old array
+        self.assertEqual(self.test_chunk2.dtype, result.dtype)
+        
+
+    def test_stdev_normalization(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
